640465e59eda48a85467bc641b5c05e3d0bdcfae
Jesse Abadilla <jesse.abadilla@image-plus.co.uk>
2024-03-19 13:26:25 +0000

    Add `git` and `wp-cli` to devenv

--- devenv/flake.nix
+++ devenv/flake.nix
@@ -9,6 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
   nixConfig = {
     extra-trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
@@ -18,6 +19,7 @@
     devenv,
     fenix,
     nixpkgs,
+    nixpkgs-unstable,
     self,
     systems,
     ...
@@ -27,6 +29,7 @@
     devShells = for-each-system (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
       in {
         default = devenv.lib.mkShell {
           inherit inputs pkgs;
@@ -41,6 +44,7 @@
                   enable = true;
                 };
               };
+              packages = [pkgs-unstable.git pkgs-unstable.wp-cli];
             })
           ];
         };
